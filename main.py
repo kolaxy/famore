08e6c4f78ead2cec61a80d6bc4d99036d52b5d44
kolaxy <vybornoff@outlook.com>
2023-11-21 14:31:52 +0300

    main py db

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from fastapi import FastAPI
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    remove_todo,
+)
+from model import Todo
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,26 +21,46 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 @app.get("/")
 def read_root():
     return {"Ping": "Pong"}
 
+
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id():
-    return 1
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.get("/api/todo/{title}", response_model=Todo)  # Fixed route path
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)  # Added 'await'
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
 
-@app.put("/api/todo{id}")
-async def put_todo(id, todo):
-    return 1
- 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, f"Something went wrong / Bad request")
+
+
+# Fixed route path and added missing closing brace
+@app.put("/api/todo/{title}", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
+
+
+@app.delete("/api/todo/{title}")  # Fixed route path
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo item!"
+    raise HTTPException(404, f"There is no TODO item with this title {title}")
